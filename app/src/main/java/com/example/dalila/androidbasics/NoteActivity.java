ccd67e50076cef98198b35be063e8c339d0788bb
Dalila Isanovic <dalila.isan@gmail.com>
2020-09-09 22:34:06 +0200

    detecting double taps on lined edit text

--- app/src/main/java/com/example/dalila/androidbasics/NoteActivity.java
+++ app/src/main/java/com/example/dalila/androidbasics/NoteActivity.java
@@ -3,13 +3,19 @@ package com.example.dalila.androidbasics;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.dalila.androidbasics.models.Note;
 import com.example.dalila.androidbasics.util.LinedEditText;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity
+        implements View.OnTouchListener,
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
     private static final String TAG = "NoteActivity";
 
     private LinedEditText mLinedEditText;
@@ -18,6 +24,7 @@ public class NoteActivity extends AppCompatActivity {
 
     private boolean mIsNewNote;
     private Note mInitialNote;
+    private GestureDetector mGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,8 @@ public class NoteActivity extends AppCompatActivity {
             //it is not a new note; VIEW MODE
             setNoteProperties();
         }
+
+        setListeners();
     }
 
     //when we enter the activity, we check whether it is a new note we are viewing
@@ -60,5 +69,63 @@ public class NoteActivity extends AppCompatActivity {
     private void setNewNoteProperties() {
         mViewTitle.setText("Note title");
         mEditTitle.setText("Note title");
+    }
+
+    private void setListeners() {
+        //setting a listener on the lined edit text; double tap on that widget for entering edit mode
+        mLinedEditText.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(this, this);
+        //generally, setting up gesture and dougle tap listners and such is to you have to use an onTouchListener and then
+        // you have to pass that touch event to the gesture listener you wanna use
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
     }
 }
