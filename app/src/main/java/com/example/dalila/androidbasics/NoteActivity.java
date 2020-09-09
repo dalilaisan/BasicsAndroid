86221453706b3ec0b145b12423c2b28f4c0377b4
Dalila Isanovic <dalila.isan@gmail.com>
2020-09-09 22:16:51 +0200

    setting note properties for note activity (view mode and edit mode)

--- app/src/main/java/com/example/dalila/androidbasics/NoteActivity.java
+++ app/src/main/java/com/example/dalila/androidbasics/NoteActivity.java
@@ -3,20 +3,62 @@ package com.example.dalila.androidbasics;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.dalila.androidbasics.models.Note;
+import com.example.dalila.androidbasics.util.LinedEditText;
 
 public class NoteActivity extends AppCompatActivity {
     private static final String TAG = "NoteActivity";
+
+    private LinedEditText mLinedEditText;
+    private EditText mEditTitle;
+    private TextView mViewTitle;
+
+    private boolean mIsNewNote;
+    private Note mInitialNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
-        if(getIntent().hasExtra("selected_note")) {
-            Note note = getIntent().getParcelableExtra("selected_note");
-            Log.d(TAG, "onCreate: " + note.toString());
+        mLinedEditText = findViewById(R.id.note_text);
+        mEditTitle = findViewById(R.id.note_edit_title);
+        mViewTitle = findViewById(R.id.note_text_title);
+
+        if (getIncomingIntent()) {
+            //it is a new note; EDIT MODE
+            setNewNoteProperties();
         }
+        else{
+            //it is not a new note; VIEW MODE
+            setNoteProperties();
+        }
+    }
+
+    //when we enter the activity, we check whether it is a new note we are viewing
+    public boolean getIncomingIntent() {
+        if (getIntent().hasExtra("selected_note")){
+            mInitialNote = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "getIncomingIntent: " + mInitialNote.toString());
+
+            mIsNewNote = false;
+            return false;
+        }
+        mIsNewNote = true;
+        return true;
+    }
+
+    private void setNoteProperties() {
+        mViewTitle.setText(mInitialNote.getTitle());
+        mEditTitle.setText(mInitialNote.getTitle());
+        mLinedEditText.setText(mInitialNote.getContent());
+    }
+
+    private void setNewNoteProperties() {
+        mViewTitle.setText("Note title");
+        mEditTitle.setText("Note title");
     }
 }
