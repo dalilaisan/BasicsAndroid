995ea8b8dbbba66d2962c1962099b479d141b9bc
Dalila Isanovic <dalila.isan@gmail.com>
2020-09-10 00:20:04 +0200

    swiping recycler view items

--- app/src/main/java/com/example/dalila/androidbasics/MainActivity.java
+++ app/src/main/java/com/example/dalila/androidbasics/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.dalila.androidbasics;
 
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -71,6 +73,9 @@ public class MainActivity extends AppCompatActivity implements NotesRecyclerAdap
         //set that decorator to the recycler view
         mRecyclerView.addItemDecoration(itemDecorator);
 
+        //attach the item touch helper to recycler view (for swiping the items)
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(mRecyclerView);
+
         //instantiate the adapter
         mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes, this);
         //lastly, set the adapter to the recycler view
@@ -94,4 +99,24 @@ public class MainActivity extends AppCompatActivity implements NotesRecyclerAdap
         Intent intent = new Intent(this, NoteActivity.class);
         startActivity(intent);
     }
+
+    private void deleteNote(Note note) {
+        mNotes.remove(note);
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    //we ue the itemtouchhelper to move/swipe items in the recycler view
+    private ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        //the onMove method is used to rearrange the items
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+            return false;
+        }
+
+        //swiping on a note will remove the note
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            deleteNote(mNotes.get(viewHolder.getAdapterPosition()));
+        }
+    };
 }
